4845c417f4fb83119cc5beab4f0f52df1dd2b284
jonson <jdeyoung@gmail.com>
2011-10-06 10:22:12 -0700

    * more robust, count the # instances but also maintain each instance of the crash  * refactored for better mongo data model

--- app/controllers.scala
+++ app/controllers.scala
@@ -7,17 +7,21 @@ import com.mongodb.casbah.Imports._
 import collection.JavaConversions._
 import collection.mutable.HashMap
 import collection.mutable.HashSet
-import model.CrashLog
 import scala.concurrent.ops.spawn
+import model.{CrashLog, CrashReport}
 
 object Mongo {
   val mongoConn = MongoConnection()
   val mongo = mongoConn("traces")
-  val mongoColl = new HashMap[String,MongoCollection]
-  mongoColl += "prod" -> mongo("prod")
-  mongoColl += "release" -> mongo("release")
-  mongoColl += "dev" -> mongo("dev")
+  val crashlogs = new HashMap[String,MongoCollection]
+  crashlogs += "prod" -> mongo("prod_logs")
+  crashlogs += "release" -> mongo("release_logs")
+  crashlogs += "dev" -> mongo("dev_logs")
 
+  val crashreports = new HashMap[String,MongoCollection]
+  crashreports += "prod" -> mongo("prod_reports")
+  crashreports += "release" -> mongo("release_reports")
+  crashreports += "dev" -> mongo("dev_reports")
 }
 
 object Application extends Controller with Logging {
@@ -35,7 +39,7 @@ object Application extends Controller with Logging {
     val isvalid = validModes(mode);
 
     if (isvalid) {
-      val traces = CrashLog.find(mode)
+      val traces = CrashReport.findCrashReports(mode)
       html.index (mode, traces);
     }
   }
@@ -45,13 +49,75 @@ object Application extends Controller with Logging {
     val isvalid = validModes(mode);
 
     if (isvalid) {
-      val trace = CrashLog.find(mode, id)
+      val trace = CrashReport.findCrashReport(mode, id)
 
       if (trace.isDefined) {
         html.trace(mode, trace.get)
       }
     }
+  }
 
+  def changeStatus(mode:String, id:String) = {
+    val isvalid = validModes(mode);
+
+    if (isvalid) {
+
+      val status = request.params.get("status");
+      log.debug("checking status")
+
+      log.debug("mode is ok")
+
+      if (status == "new" || status == "old") {
+
+        log.debug("status is ok")
+
+        val query = MongoDBObject("_id" -> new ObjectId(id))
+        val inc = $set("status" -> status.toUpperCase)
+        val obj = Mongo.crashreports(mode).findAndModify(query, inc)
+
+        if (obj.isDefined) {
+          // now get the new status
+          val newStatus = obj.get.getAs[String]("status").get
+
+          log.debug("new status=" + newStatus)
+
+          crashreport(mode, id)
+        }
+      }
+
+    }
+
+  }
+
+  def crashreport(mode: String, id: String) = {
+
+    log.info("im here")
+    val isvalid = validModes(mode);
+
+    if (isvalid) {
+      val crashReport = CrashReport.findCrashReport(mode, id)
+
+
+      if (crashReport.isDefined) {
+
+        val report = crashReport.get
+        val buffer = new scala.collection.mutable.ListBuffer[CrashLog]
+
+        report.instances.foreach(instance => {
+          val logOpt = CrashReport.findCrashLog(mode, instance.toString)
+          if (logOpt.isDefined) {
+            buffer += logOpt.get
+          }
+        })
+
+        log.info("crash report defined")
+        html.crashreport(mode, crashReport.get, buffer.toList)
+      } else {
+        log.info("crash report not defined: " + id)
+      }
+    } else {
+      log.info("invalid mode: " + mode)
+    }
   }
 
 }
@@ -77,10 +143,7 @@ object Collector extends Controller with Logging {
     log.debug("post request received")
 
     // strip out the body param, seems to be included
-    val params = request.params.allSimple filter (param => !param._1.eq("body") && CrashLog.validField(param._1))
-
-    // temp disable valid params check
-
+    val params = request.params.allSimple filter (param => !param._1.eq("body") && CrashReport.validField(param._1))
 
     // take a copy, not sure how safe the params object is in play
     val paramsCopy = collection.immutable.Map() ++ params
@@ -89,7 +152,9 @@ object Collector extends Controller with Logging {
     spawn {
       // there must be a better way to do this, we could use a thread pool, ...  actor??
       log.info("Starting processing")
-      CrashLog.save(mode, paramsCopy)
+
+
+      CrashReport.save(mode, paramsCopy)
       log.info("Processed")
     }
 
--- app/model/CrashLog.scala
+++ app/model/CrashLog.scala
@@ -9,7 +9,7 @@ import util.Logging
 import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
 
-object CrashLog extends Logging {
+object CrashReport extends Logging {
 
   private val validCrashFields = new HashSet[String]
   validCrashFields += "USER_CRASH_DATE"
@@ -30,41 +30,75 @@ object CrashLog extends Logging {
   /**
    * Fetches all logs in the system
    */
-  def find (mode: String) : List[CrashLog] = {
+  def findCrashReports (mode: String) : List[CrashReport] = {
     // fetch the data (transform this to a List[CrashLog])
-    val objs : Iterator[CrashLog] = for (obj <- Mongo.mongoColl(mode).find().sort(MongoDBObject("lastDate" -> -1))) yield {
-      // json serialization would be better
-      val cl = new CrashLog
-      cl.stackTrace = obj.getAs[String]("STACK_TRACE")
-      cl.date = obj.getAs[DateTime]("USER_CRASH_DATE")
-      cl.id = obj.getAs[ObjectId]("_id")
-      cl.count = obj.getAs[Int]("count")
-      cl.lastDate = obj.getAs[DateTime]("lastDate")
-      cl
+    val objs : Iterator[CrashReport] = for (obj <- Mongo.crashreports(mode).find().sort(MongoDBObject("lastDate" -> -1))) yield {
+
+      // get a few mandatory fields
+      val id = obj.getAs[ObjectId]("_id").get
+      val stackTrace = obj.getAs[String]("stackTrace").get
+
+      var cr = new CrashReport(id, stackTrace)
+      cr.originalDate = obj.getAs[DateTime]("originalDate")
+      cr.lastDate = obj.getAs[DateTime]("lastDate")
+      cr.status = obj.getAs[String]("status").getOrElse("NEW")
+      var instances = obj.getAs[BasicDBList]("instances").get.toList
+      var aslist = instances.collect { case s: ObjectId => s}
+      cr.instances = aslist
+      cr
     }
 
     return objs.toList
   }
 
-  def find (mode: String, id : String ) : Option[CrashLog] = {
-    val query = MongoDBObject("_id" -> new ObjectId(id))
-    val obj = Mongo.mongoColl(mode).findOne(query)
-    if (obj.isDefined) {
-      val cl = new CrashLog
-      cl.androidVersion = obj.get.getAs[String]("ANDROID_VERSION")
-      cl.stackTrace = obj.get.getAs[String]("STACK_TRACE")
-      cl.model = obj.get.getAs[String]("PHONE_MODEL")
-      cl.display = obj.get.getAs[String]("DISPLAY")
-      cl.date = obj.get.getAs[DateTime]("USER_CRASH_DATE")
-      cl.id = obj.get.getAs[ObjectId]("_id")
-      cl.count = obj.get.getAs[Int]("count")
-      cl.lastDate = obj.get.getAs[DateTime]("lastDate")
-      cl.developerMode = obj.get.getAs[String]("DEVELOPER_MODE")
-      cl.mode = obj.get.getAs[String]("MODE")
+  def findCrashLog(mode: String, id: String) : Option[CrashLog] = {
 
-      // TODO: missing APP_VERSION_CODE from ACRA
+    val query = MongoDBObject("_id" -> new ObjectId(id))
+    val objOption = Mongo.crashlogs(mode).findOne(query)
+    if (objOption.isDefined) {
+
+      val obj = objOption.get
+
+      val cl = new CrashLog()
+
+      cl.androidVersion = obj.getAs[String]("ANDROID_VERSION")
+      cl.version = obj.getAs[String]("APP_VERSION_NAME")
+      cl.stackTrace = obj.getAs[String]("STACK_TRACE")
+      cl.model = obj.getAs[String]("PHONE_MODEL")
+      cl.display = obj.getAs[String]("DISPLAY")
+      cl.date = obj.getAs[DateTime]("USER_CRASH_DATE")
+      cl.id = obj.getAs[ObjectId]("_id")
+      cl.developerMode = obj.getAs[String]("DEVELOPER_MODE")
+      cl.mode = obj.getAs[String]("MODE")
 
       return Option(cl)
+    }
+
+    return None
+  }
+
+  def findCrashReport (mode: String, id : String ) : Option[CrashReport] = {
+    val query = MongoDBObject("_id" -> new ObjectId(id))
+    val objOption = Mongo.crashreports(mode).findOne(query)
+    if (objOption.isDefined) {
+
+      val obj = objOption.get
+
+      // get a few mandatory fields
+      val id = obj.getAs[ObjectId]("_id").get
+      val stackTrace = obj.getAs[String]("stackTrace").get
+
+      val cr = new CrashReport(id, stackTrace)
+
+      cr.originalDate = obj.getAs[DateTime]("originalDate")
+      cr.lastDate = obj.getAs[DateTime]("lastDate")
+      cr.status = obj.getAs[String]("status").getOrElse("NEW")
+
+      var instances = obj.getAs[BasicDBList]("instances").get.toList
+      var aslist = instances.collect { case s: ObjectId => s}
+      cr.instances = aslist
+
+      return Option(cr)
     }
 
     return None
@@ -74,11 +108,12 @@ object CrashLog extends Logging {
    * Current # of logs in the system
    */
   def count(mode : String) : Long = {
-    return Mongo.mongoColl(mode).count
+    return Mongo.crashreports(mode).count
   }
 
   /**
-   * Saves an object to the db
+   * Saves an object to the db.  If an existing CrashReport with the same stack trace exists, that one
+   * will be updated, otherwise, a new one will be created with this log.
    */
   def save (mode: String, params : Map[String,String]) : Unit = {
 
@@ -89,30 +124,42 @@ object CrashLog extends Logging {
       val stackTrace = params.get("STACK_TRACE")
 
       if (stackTrace.isDefined) {
-        // do a query
-        log.info("Checking for existing stack trace");
-        val query = MongoDBObject ("STACK_TRACE" -> stackTrace)
-        val inc = $inc("count" -> 1) ++ $set("lastDate" -> ISODateTimeFormat.dateTime().print(new DateTime()))
-        val existing = Mongo.mongoColl(mode).findAndModify(query, inc);
+
+        // create a new object
+        val builder = MongoDBObject.newBuilder
+        params foreach (param => {
+            builder += param._1 -> param._2
+        })
+
+        log.info("Saving new trace")
+        val stobj = builder.result()
+        Mongo.crashlogs(mode).insert(stobj)
+
+        // need the object id from crashlogs
+        val id = stobj.getAs[ObjectId]("_id").get
+
+        val query = MongoDBObject ("stackTrace" -> stackTrace)
+
+        val now = ISODateTimeFormat.dateTime().print(new DateTime())
+        val inc = $set("lastDate" -> now) ++ $set("status" -> "new") ++ $push("instances" -> id)
+
+        val existing = Mongo.crashreports(mode).findAndModify(query, inc)
 
         if (existing.isDefined) {
-          log.info("Existing stack trace found and count incremented");
+          log.info("Existing crash report found and updated");
         } else {
           log.info("New stack trace found")
 
           // create a new object
           val builder = MongoDBObject.newBuilder
-          params foreach (param => {
-              builder += param._1 -> param._2
-          })
+          builder += "status" -> "NEW"
+          builder += "instances" -> List(id)
+          builder += "originalDate" -> now
+          builder += "lastDate" -> now
+          builder += "stackTrace" -> stackTrace
 
-          builder += "count" -> 1
-          builder += "status" -> "new"
-          builder += "lastDate" -> ISODateTimeFormat.dateTime().print(new DateTime())
-          val updateObj = builder.result()
-
-          log.info("Saving new trace")
-          Mongo.mongoColl(mode).insert(updateObj);
+          log.info("Saving new crash report")
+          Mongo.crashreports(mode).insert(builder.result());
         }
       }
 
@@ -124,16 +171,57 @@ object CrashLog extends Logging {
 
 }
 
+/**
+ * Represents an individual crash report from the field.
+ */
 class CrashLog  {
   var id : Option[ObjectId] = None
-  var stackTrace : Option[String] = None
   var androidVersion : Option[String] = None
   var version : Option[String] = None
   var model : Option[String] = None
   var display : Option[String] = None
   var date : Option[DateTime] = None
-  var count : Option[Int] = None
-  var lastDate : Option[DateTime] = None
   var mode : Option[String] = None
   var developerMode : Option[String] = None
+  var stackTrace : Option[String] = None
 }
+
+/**
+ * Represents statics for a particular crash.
+ */
+case class CrashReport(id : ObjectId, stackTrace : String) {
+
+  /**
+   * Ordered list of individual reports
+   */
+  var instances : List[ObjectId] = List()
+
+  /**
+   * The status of the report ("NEW" or "OLD")
+   */
+  var status : String = "NEW"
+
+  /**
+   * Original date it was reported
+   */
+  var originalDate : Option[DateTime] = None
+
+  /**
+   * Last date it was reported
+   */
+  var lastDate : Option[DateTime] = None
+
+  def numReports() : Int = {
+    return instances.size()
+  }
+
+  def summary() : String = {
+    val StackParser = """(\w+):.*""".r
+    stackTrace match {
+      case StackParser(aaa) => return aaa;
+      case _ => return "UNKNOWN";
+    }
+  }
+
+}
+
